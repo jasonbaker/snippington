f735b9ca0f1683920c691b1c0efeef8fb082a097
Jason Baker <amnorvend@gmail.com>
2010-11-29 18:19:46 -0800

    Making this association work

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -4,10 +4,8 @@ class Snippet < ActiveRecord::Base
   @@title_re = /[a-zA-Z0-9\-_].*/
 
   validates :code, :presence => true
-  validates_format_of :title, :with => @@title_re, :message => "Must match #{@@title_re.inspect}"
-  validates :title, :presence => true
 
-  belongs_to :users, :foreign_key => "user_id", :class_name => "User"
+  belongs_to :user, :foreign_key => "user_id", :class_name => "User"
   has_many :tag_snippets
   has_many :tags, :through => :tag_snippets
 
