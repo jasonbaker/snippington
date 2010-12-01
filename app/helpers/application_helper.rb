f099177f1bd198d1208efce397fb01dd22ec50af
Jason Baker <amnorvend@gmail.com>
2010-11-30 23:27:12 -0800

    Adding head.js

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def head_js_include(*js_files, &proc)
+    paths = js_files.map {|p| javascript_path p}
+    text = "<script>head.js("
+    paths.each do |p|
+      text << "\"#{p}\","
+    end
+    if block_given?
+      text << "function(){\n"
+      text << capture(&proc)
+      text << "}\n"
+    end
+    text.chomp! ?,
+    text << ");</script>"
+    return raw(text)
+  end
 end
